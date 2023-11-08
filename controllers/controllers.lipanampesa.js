a7347d413fd929373bb3a5dd423534f4a6f717a4
Timoth Kibunja <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-08 21:52:31 +0300

    Update controllers.lipanampesa.js

--- controllers/controllers.lipanampesa.js
+++ controllers/controllers.lipanampesa.js
@@ -6,7 +6,7 @@ import ngrok from 'ngrok'
 // @desc initiate stk push
 // @method POST
 // @route /stkPush
-//updated
+//update
 // @access public
 export const initiateSTKPush = async(req, res) => {
     try{
