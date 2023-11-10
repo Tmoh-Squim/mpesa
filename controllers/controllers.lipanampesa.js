33fff268058b2bc0bb8f66408b9a1f439520d752
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2023-11-10 16:35:41 +0300

    updated

--- controllers/controllers.lipanampesa.js
+++ controllers/controllers.lipanampesa.js
@@ -5,7 +5,7 @@ import ngrok from 'ngrok'
 
 // @desc initiate stk push
 // @method POST
-// @route 
+// @route /stkPush
 export const initiateSTKPush = async(req, res) => {
     try{
         console.log("Request body",req.body)
