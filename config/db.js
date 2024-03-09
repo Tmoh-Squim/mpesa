4f923e15fb7c613f59bb7933527ef655eef5f57e
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2024-03-09 11:52:13 +0300

    updated

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = () => {
     mongoose
-      .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(process.env.MONGO_URI)
       .then((data) => {
         console.log(`mongod connected with server: ${data.connection.host}`);
       });
