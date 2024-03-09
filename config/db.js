32caaa42c7731676980449c395d89f77a723d221
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2024-03-09 11:44:06 +0300

    updated

--- config/db.js
+++ config/db.js
@@ -1,17 +1,14 @@
 import mongoose from "mongoose"
 
-const connectDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
-        .then(console.log('db connected')
-        )
-    } catch (error) {
-        console.log(error.message);
-        
-    }
-}
+const connectDB = () => {
+    mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((data) => {
+        console.log(`mongod connected with server: ${data.connection.host}`);
+      });
+  };
 
 export default connectDB
